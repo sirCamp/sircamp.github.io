9574631d9d7a95843cf216d8c074f32a6da899ca
sirCamp <stefano@shellrent.com>
2015-04-10 15:32:11 +0200

    introduzione angular

--- js/init.js
+++ js/init.js
@@ -6,3 +6,44 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+//APP
+var sircamp = angular.module('sircamp', ['ngRoute']);
+
+
+//ROUTE
+sircamp.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+    .when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'mainController'
+    })
+
+    // route for the about page
+   /* .when('/about', {
+        templateUrl : 'pages/about.html',
+        controller  : 'aboutController'
+    })*/
+
+    // route for the contact page
+    .when('/contact', {
+        templateUrl : 'pages/contact.html',
+        controller  : 'contactController'
+    });
+});
+
+
+//CONTROLLER
+sircamp.controller('mainController', function($scope) {
+
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+
+sircamp.controller('contactController', function($scope) {
+
+	$scope.message = 'Everyone come and see how good I look!';
+});
